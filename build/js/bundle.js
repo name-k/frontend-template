e767825e6d613e1e4262907dafbef8e60ece60ba
name-k <psykeonfarm@gmail.com>
2016-02-01 14:40:01 +0700

    removed build folder from the repo

--- build/js/bundle.js
+++
@@ -1,1 +0,0 @@
-var bundle=function(r){function t(n){if(e[n])return e[n].exports;var o=e[n]={exports:{},id:n,loaded:!1};return r[n].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var e={};return t.m=r,t.c=e,t.p="/js/",t(0)}([function(r,t){"use strict"}]);
