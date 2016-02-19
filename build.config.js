99488ad4de3a34262b35c25c70bde4ab1d198500
name-k <psykeonfarm@gmail.com>
2016-02-20 02:56:20 +0700

    new build paths

--- build.config.js
+++ build.config.js
@@ -1,53 +1,69 @@
 'use strict';
 
-let dir = {};
+const path = require('path');
 
+let dir = {};
 dir.root   = './';
-dir.src    = dir.root + 'src/';
-dir.build  = dir.root + 'build/';
-dir.stl    = dir.src + 'stl/';
-dir.tpl    = dir.src + 'tpl/';
-dir.js     = dir.src + 'js/';
-dir.fonts  = dir.src + 'fonts/';
-dir.img    = dir.src + 'img/';
-dir.dsgn   = dir.src + 'img/design/';
-dir.svg    = dir.src + 'img/svg/';
-dir.imggag = dir.src + 'img/gag/';
+dir.src    = path.join(dir.root, 'src');
+dir.build  = path.join(dir.root, 'build');
+dir.stl    = path.join(dir.src, 'stl');
+dir.tpl    = path.join(dir.src, 'tpl');
+dir.js     = path.join(dir.src, 'js');
+dir.fonts  = path.join(dir.src, 'fonts');
+dir.img    = path.join(dir.src, 'img');
+dir.svg    = path.join(dir.src, 'svg');
+dir.dsgn   = path.join(dir.src, 'img/design');
+dir.svg    = path.join(dir.src, 'img/svg');
+dir.imggag = path.join(dir.src, 'img/pic');
+
+let flags = {};
+flags.mode        = process.env.NODE_ENV || 'dev';
+flags.isDev       = !process.env.NODE_ENV || process.env.NODE_ENV == 'dev';
+flags.isProd      = process.env.NODE_ENV == 'prod';
+flags.debug       = process.env.NODE_DEBUG == 'true';
+flags.shouldWatch = !process.env.NODE_WATCH || process.env.NODE_WATCH == 'true';
 
 module.exports = {
-  dir : dir,
+  dir, flags,
   paths : {
     js : {
-      // all js paths are in webpack config
+      context      : path.resolve(process.cwd()),
+      rootJSPath   : path.resolve(process.cwd(), dir.js),
+      publicPath   : '/js/',
+      dest         : path.join(dir.build, 'js'),
     },
     styles: {
-      watch   : dir.stl + '**/*.*',
-      process : dir.stl + 'main.*',
-      build   : dir.build + 'css/',
+      watch : path.join(dir.stl, '**/*.*'),
+      src   : path.join(dir.stl, 'main.*'),
+      dest  : path.join(dir.build, 'css'),
     },
     templates : {
-      watch   : dir.tpl + '**/*.*',
-      process : dir.tpl + '!(_)*.jade',
-      build   : dir.build,
+      watch : dir.tpl + '**/*.*',
+      src   : dir.tpl + '!(_)*.jade',
+      dest  : dir.build,
     },
     fonts : {
-      process : dir.fonts + '**.*',
-      build   : dir.build + 'fonts/',
+      src  : path.join(dir.fonts, '**.*'),
+      dest : path.join(dir.build, 'fonts'),
     },
     img : {
-      watch   : dir.dsgn + '**/*.@(jpg|jpeg|png|gif)',
-      process : dir.dsgn + '**/*.@(jpg|jpeg|png|gif)',
-      build   : dir.build + 'img/',
-      svg : {
-        watch   : dir.svg + '**/*.svg',
-        process : dir.svg + '**/*.svg',
-        build   : dir.build + 'img/svg/',
+      watch : path.join(dir.dsgn, '**/*.@(jpg|jpeg|png|gif)'),
+      src   : path.join(dir.dsgn, '**/*.@(jpg|jpeg|png|gif)'),
+      dest  : path.join(dir.build, 'img'),
+    },
+    imggag : {
+      watch : path.join(dir.imggag, '**/*.@(jpg|jpeg|png|gif)'),
+      src   : path.join(dir.imggag, '**/*.@(jpg|jpeg|png|gif)'),
+      dest  : path.join(dir.build, 'pic'),
+    },
+    svg : {
+      watch : path.join(dir.svg, '**/*.svg'),
+      src   : path.join(dir.svg, '**/*.svg'),
+      dest  : path.join(dir.build, 'svg'),
+      templates : {
+        svg : path.resolve(dir.img, 'svg-sprite-templates/svg-sprite.svg'),
+        css : path.resolve(dir.img, 'svg-sprite-templates/svg-sprite.css'),
       },
-      gag : {
-        watch   : dir.imggag + '**/*.@(jpg|jpeg|png|gif)',
-        process : dir.imggag + '**/*.@(jpg|jpeg|png|gif)',
-        build   : dir.build + 'pic/',
-      }
     },  
   }
   
