da1faa71db174c6cc4e92ab8f902e5688d85b26c
nmk macbook-12 <psykeonfarm@gmail.com>
2016-02-24 20:30:28 +0700

    minor bugfix in path

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ lazyTask('jade', './tasks/templates', {
 });
 lazyTask('jade:mixins', './tasks/collectJadeMixins', {
   dir : path.join(config.dir.tpl, 'mixins'),
-  collectionName : '_all'
+  outputFile : path.resolve(process.cwd(), config.dir.tmp, 'templates/mixins.jade')
 });
 gulp.task('templates', gulp.series('jade:mixins', 'jade'));
 
